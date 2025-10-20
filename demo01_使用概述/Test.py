7b622454809712a8f337a21bfdb78a202455b614
leon_hxk <leon_hxk@163.com>
2025-10-20 14:28:42 +0800

    sgg-langchain从入门到实战-入门简介

---
+++ demo01_使用概述/Test.py
@@ -0,0 +1,17 @@
+import sys
+
+import langchain
+import openai
+
+print(langchain.__version__)  # 0.3.27
+
+print(openai.__version__)  # 1.109.1
+
+print(sys.version)  # 3.10.18 | packaged by Anaconda, Inc. | (main, Jun  5 2025, 13:08:55) [MSC v.1929 64 bit (AMD64)]
+
+
+## OPENAI官方
+# # https://platform.openai.com/settings/organization/api-keys 在openai地址创建（可咨询豆包）
+# OPENAI_API_KEY1="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"
+# # 这是通用API基础地址
+# OPENAI_BASE_URL="https://api.openai.com/v1"
